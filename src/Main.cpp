ea590efc3435ad238dfac7ad8937899b33e5831b
kdr250 <54400975+kdr250@users.noreply.github.com>
2025-11-07 15:04:52 +0900

    display window

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    // initialize glfw
+    if (glfwInit() == GL_FALSE) {
+        return EXIT_FAILURE;
+    }
 
-    return 0;
+    // create window
+    GLFWwindow* window = glfwCreateWindow(640, 480, "PhongShaderSample", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    // specify version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // create context
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    // initialize glew
+    if (glewInit() != GLEW_OK) {
+        return EXIT_FAILURE;
+    }
+
+    // game loop
+    while (glfwWindowShouldClose(window) == GL_FALSE) {
+        // clear buffer
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // swap double buffer
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // terminate glfw
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
 }
