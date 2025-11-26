066db9cd9099927c117499fdf9683982b3fee0e9
kdr250 <54400975+kdr250@users.noreply.github.com>
2025-11-26 16:29:40 +0900

    index buffer を使わないようにする

--- src/Main.cpp
+++ src/Main.cpp
@@ -38,13 +38,13 @@ bool createVertexArray() {
     float vertices[] = {
         -0.5f, 0.5f,  0.0f, 0.0f, 0.0f, 1.0f, // top left
         0.5f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f,  // top right
-        0.5f,  -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f // bottom left
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
+        0.5f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f,  // top right
+        0.5f,  -0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // bottom right
     };
     // clang-format on
-    unsigned int numVerts = 4;
-    unsigned int indices[] = {0, 1, 2, 2, 3, 0};
-    unsigned int numIndices = 6;
+    unsigned int numVerts = 6;
 
     // Create vertex array
     glGenVertexArrays(1, &vertexArray);
@@ -54,12 +54,6 @@ bool createVertexArray() {
     glGenBuffers(1, &vertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     glBufferData(GL_ARRAY_BUFFER, numVerts * 6 * sizeof(float), vertices, GL_STATIC_DRAW);
-
-    // Create index buffer
-    glGenBuffers(1, &indexBuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-    glBufferData(
-        GL_ELEMENT_ARRAY_BUFFER, numIndices * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     // Specify the vertex attributes
     glEnableVertexAttribArray(0); // position
@@ -212,7 +206,7 @@ void mainLoop() {
     setUniformMatrix4("uProjection", projection);
     setUniformVec3("uCameraPos", cameraPos);
 
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 
     // swap double buffer
     glfwSwapBuffers(window);
